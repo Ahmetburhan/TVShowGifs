a0b8c46c17eeae3f881df8650b1101d882df3d36
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2016-09-01 23:20:16 -0400

    added rating and hover function

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
 var showTitle = ['The Americans', 'Orphan Black', 'Game of Thrones', 'Sherlock', 'Mr. Robot', 'The Walking Dead', 'Daredevil', 'Unbreakable Kimmy Schmidt', 'Silicon Valley', 'The Flash', 'Supergirl', 'Louie', 'iZombie', 'Homeland', 'Arrow', 'Veep', 'Parks and Recreation'];
+
 //creates buttons
 function createButtons(){
 	$('#TVButtons').empty();
-
+	var embedGif; var paused;
 	for(var i = 0; i < showTitle.length; i++){
-		var showBtn = $('<button>');
-		showBtn.text(showTitle[i]);
-		showBtn.addClass('showBtn');
-		showBtn.attr({'data-name': showTitle[i]});
+		var showBtn = $('<button>').text(showTitle[i]).addClass('showBtn').attr({'data-name': showTitle[i]});
 		$('#TVButtons').append(showBtn);
 	}
-
+	
 	//displays gifs on click
 	$('.showBtn').on('click', function(){
 		$('.display').empty();
 		
-		var thisShow = $(this).data('name').replace(/\s/g, '+');
+		var thisShow = $(this).data('name');
 		var giphyURL = "http://api.giphy.com/v1/gifs/search?q=tv+show+" + thisShow + "&limit=10&api_key=dc6zaTOxFJmzC";
 		$.ajax({url: giphyURL, method: 'GET'}).done(function(giphy){
-		var currentGif = giphy.data;
+			var currentGif = giphy.data;
 			$.each(currentGif, function(index,value){
-			var embedGif = value.images.original.url;
-			var newGif = $('<img>');
-			newGif.attr('src', embedGif);
-			$('.display').append(newGif);
+				embedGif= value.images.original.url;
+				paused = value.images.original_still.url;
+				var rating = $('<div>').html('Rating: '+value.rating);
+				var stillGif = $('<img>').attr('src', paused).addClass('playOnHover');
+				$('.display').append(rating, stillGif);
 			});
 		});
-	});
-}
+	}); 
+
+	//plays gif on hover *WIP
+	$(".playOnHover").hover(
+		function(){
+	       	$(this).attr('src', embedGif);
+	    },
+	    function() {
+	        $(this).attr('src', paused);
+	    }                       
+	);  
+}   	 
+
 //sets a button from input
 $('#addShow').on('click', function(){
 	var newShow = $('#newShowInput').val().trim();
@@ -37,6 +47,3 @@ $('#addShow').on('click', function(){
 });
 
 createButtons();
-
-
-//add rating, make moving gif on click
